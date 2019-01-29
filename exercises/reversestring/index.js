1b30d9f6c162d2d7df855e12cc632e150adca719
Jacqueline Chenault <adigitalnative@gmail.com>
2019-01-29 08:34:35 -0500

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // // V1 using .reverse()
+  // return str.split('').reverse().join('')
+
+
+  // V2 using a for loop
+  let reversed = ""
+
+  for(let char of str) {
+    reversed = char + reversed
+  }
+  return reversed
+
+  // // V3 using array helper
+  // return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
