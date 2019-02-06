2caaf4d73d0bd4dab00a2a4274768e1658ee1257
Jacqueline Chenault <adigitalnative@gmail.com>
2019-02-06 15:56:16 -0500

    MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,43 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  // // My Solution
+  // let map = {}
+  // let maxChar = {
+  //   char: "",
+  //   value: 0
+  // }
+  //
+  // str.split('').forEach(char => {
+  //   map[char] ? map[char] += 1 : map[char] = 1
+  // })
+  //
+  // Object.keys(map).forEach(char => {
+  //   if(map[char] > maxChar.value) {
+  //     maxChar.char = char
+  //     maxChar.value = map[char]
+  //   }
+  // })
+  //
+  // return maxChar.char
+
+  //Given Solution
+  const charMap = {}
+  let max = 0
+  let maxChar = ""
+
+  for(const char of str) {
+    charMap[char] = charMap[char] + 1 || 1 // Cool way to do this without an if stmt
+
+    if(charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
