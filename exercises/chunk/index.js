1f02a5e22cd49ae1c7fed8bd1053451cd2b0c558
Jacqueline Chenault <adigitalnative@gmail.com>
2019-02-07 11:52:20 -0500

    Chunked Array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+
+  // // V1
+  // let chunked = []
+  //
+  // for(const element of array) {
+  //   const last = chunked[chunked.length - 1]
+  //
+  //   if(!last || last.length === size) {
+  //     chunked.push([element])
+  //   } else {
+  //     last.push(element)
+  //   }
+  // }
+  //
+  // return chunked
+
+  // V2
+
+  let chunked = []
+  let index = 0
+
+  while(index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
+  return chunked
+
+  // while the index is less than array.length
+    // slice the array at index + size
+    // iterate index by size
+
+}
 
 module.exports = chunk;
