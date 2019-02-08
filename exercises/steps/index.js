2418132710d85e6e2410e8d800776a82d6df2443
Jacqueline Chenault <adigitalnative@gmail.com>
2019-02-08 14:28:59 -0500

    Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// //Simple version
+// function steps(n) {
+//
+//   counter = 1
+//   while(counter <=n) {
+//     console.log('#'.repeat(counter) + ' '.repeat(n-counter))
+//     counter++
+//   }
+//
+// }
+
+// Recursive Version
+function steps(n, row=0, stair='') {
+  if (n === row) {
+    return
+  }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+
+  steps(n, row, stair)
+
+}
 
 module.exports = steps;
